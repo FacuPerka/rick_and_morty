efb1b4591698ce145ed09322ba7bd6e19391a528
Facundo Perka <perkafacund@gmail.com>
2023-04-19 11:59:05 -0300

    Fixed About Me "Errors".

--- Client/src/components/About.jsx
+++ Client/src/components/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>Culo</h1>
-      <img src="https://pbs.twimg.com/media/FD8ybS6UcBMfUpk.png" alt="Tu imagen" />
+      <h1>About Me</h1>
+      <img src="https://media.discordapp.net/attachments/1087772847428075560/1098259508511391874/unknown.png?width=383&height=430" alt="Tu imagen" />
     </div>
   );
 };
