7db1567a7fc149f5f661668b5ccf3a5a473c2a06
Facundo Perka <perkafacund@gmail.com>
2023-04-21 09:06:02 -0300

    getCharById Implementation.

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,10 +1,27 @@
 const axios = require('axios');
 
 const getCharById = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`);
-    .then(response => response.data)
-    .then(data => {}
-}
+    axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(response => {
+        const { name, gender, species, origin, image, status } = response.data;
+        const character = {
+          id,
+          name,
+          gender,
+          species,
+          origin: origin.name || 'unknown', // Usamos la notación de punto y agregamos un valor predeterminado para el caso en que el nombre no exista
+          image,
+          status
+        };
+        res.send(character);
+      })
+      .catch(error => {
+        console.log(error);
+        res.sendStatus(404);
+      });
+  };
+  
+  
 
 module.exports = {
     getCharById
